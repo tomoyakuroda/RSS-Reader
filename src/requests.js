4d120696559025cc37abc5e41229cd69e32045c9
Tomoya <200378857@student.georgianc.on.ca>
2019-11-23 01:09:03 -0500

    Fix url

--- src/requests.js
+++ src/requests.js
@@ -4,8 +4,8 @@ const corsUrl="https://api.rss2json.com/v1/api.json?rss_url=";
 export const getFeedURL=async (inputURL)=>{
 
     let feedURL = ''
-    // /.netlify/functions/getRSSFeedURL
-    const res = await axios.post('https://youthful-villani-c8113a.netlify.com/.netlify/functions/getRSSFeedURL',{url:inputURL})
+
+    const res = await axios.post('/.netlify/functions/getRSSFeedURL',{url:inputURL})
         feedURL=res.data
     return feedURL;
 }
