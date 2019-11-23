328452ce09d40042dc29f082f5cedf5b76a999d3
Tomoya <200378857@student.georgianc.on.ca>
2019-11-22 21:11:57 -0500

    Remove offline mode

--- src/requests.js
+++ src/requests.js
@@ -4,8 +4,7 @@ const corsUrl="https://api.rss2json.com/v1/api.json?rss_url=";
 export const getFeedURL=async (inputURL)=>{
 
     let feedURL = ''
-    let requestURL= process.env.REACT_APP_MODE === 'local' ? "http://localhost:9000/getRSSFeedURL" : '/.netlify/functions/getRSSFeedURL'
-    const res = await axios.post(requestURL,{url:inputURL})
+    const res = await axios.post('/.netlify/functions/getRSSFeedURL',{url:inputURL})
         feedURL=res.data
     return feedURL;
 }
